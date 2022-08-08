f0db6a95c65a6aa1164d49134007ce096d6f08c5
baraaj <jeridibaraa@gmail.com>
2022-08-08 11:35:27 +0200

    m commit

--- router/club.js
+++ router/club.js
@@ -5,12 +5,13 @@
  const ClubCtrl=require('../controllers/club');
 
 
- //router.post('/',auth,ClubCtrl.ajoutClub);
-router.post('/',ClubCtrl.ajoutClub);
+ //router.post('/',auth,multer/ClubCtrl.ajoutClub);
+router.post('/',multer,ClubCtrl.ajoutClub);
 router.put("/:id",ClubCtrl.updateClub);
 router.delete("/:id",ClubCtrl.deleteClub);
 router.get("/:id",ClubCtrl.getOneClub);
 router.get("/",ClubCtrl.getAllClubs);
 //router.get("/",ClubCtrl.getClubByRegion);
 router.get("/",ClubCtrl.getClubByGovernement);
+app.use('/images', express.static(path.join(__dirname, 'images')));
  module.exports=router;
