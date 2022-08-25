bbd224bc776a7f4fb4ec4c8e6a0ee4fe5205a558
baraaj <jeridibaraa@gmail.com>
2022-08-25 03:06:25 +0200

    f commit

---
+++ router/news.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const auth=require('../middleware/auth');
+const router=express.Router();
+const ClubCtrl=require('../controllers/club');
+const multer=require(  '../middleware/multer-config');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=router;
